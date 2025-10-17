7764e595ea21a0a22d519c3573d5349ba963074f
liaoyf <yfliao@ifintech.ltd>
2025-10-17 15:44:39 +0800

    fix: 修改依赖

--- android/src/main/java/com/dylanvann/fastimage/FastImageGlideModule.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageGlideModule.java
@@ -6,4 +6,8 @@ import com.bumptech.glide.module.AppGlideModule;
 // We need an AppGlideModule to be present for progress events to work.
 @GlideModule
 public final class FastImageGlideModule extends AppGlideModule {
+    @Override
+    public boolean isManifestParsingEnabled() {
+        return false;
+    }
 }
